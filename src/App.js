6853e63769d2d8b2eb59371c0cc468ede73a9e72
Aaron Waguespack <Aaron_Waguespack@hotmail.com>
2019-03-31 14:05:24 -0600

    uploading to heroku

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://facerecognition-api-aw.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -96,7 +96,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if(response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://facerecognition-api-aw.herokuapp.com/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
